480660336657a17ecab7f8f892435b547b230ed8
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2023-04-07 03:55:50 -0300

    feat: adicionado menu e páginas home e sobre

--- site-bambui/src/app/app-routing.module.ts
+++ site-bambui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/pages/home/home.component';
+import { SobreComponent } from './shared/pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
